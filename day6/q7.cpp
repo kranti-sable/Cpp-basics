96b798c720fc7ee70304b218edea8d1d9080a95d
kranti-sable <krantisable3@gmail.com>
2026-03-15 00:43:37 +0530

    day6 lec code

---
+++ day6/q7.cpp
@@ -0,0 +1,22 @@
+/* print 
+        *
+      * *
+    * * *
+  * * * *     */
+#include <iostream>
+using namespace std;
+int main(){
+    int n=4;
+    for(int i=1;i<=n;i++){
+        //space
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
+        }
+        //stare
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
