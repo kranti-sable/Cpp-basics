20f9442548b768f7ad041d5e50878076f5d8b3fe
kranti-sable <krantisable3@gmail.com>
2026-03-19 21:32:42 +0530

    day9 lec code

---
+++ day9/q1.cpp
@@ -0,0 +1,12 @@
+//what will ptr2 point to in the following code
+#include <iostream>
+using namespace std;
+int main(){
+    int x=5,y=10;
+    int *ptr1=&x,*ptr2=&y;
+    ptr2=ptr1;
+    cout<<ptr2<<"\n";
+    cout<<ptr1<<"\n";
+    cout<<&x<<"\n";
+    return 0;
+} 
