2632987d8b4eacbfa771efb4717248c18c52e470
kranti-sable <krantisable3@gmail.com>
2026-03-25 21:19:11 +0530

    day10 lec code

---
+++ day10/q8.cpp
@@ -0,0 +1,24 @@
+// print max subarrays sum 
+#include <iostream>
+using namespace std;
+void maxsubarraysum(int arr[],int n){
+    int maxsum=INT_MIN;
+    for(int start=0;start<n;start++){
+        for(int end=start;end<n;end++){
+           int currsum=0;
+            for(int i=start;i<=end;i++){
+                currsum+=arr[i];
+            }
+            cout<<currsum<<", ";
+            maxsum=max(maxsum,currsum);
+        }
+        cout<<endl;
+    }
+    cout<<"maximum subarray sum="<<maxsum<<endl;
+}
+int main(){
+    int arr[]={2,-3,6,-5,4,2};
+    int n=sizeof(arr)/sizeof(int);
+    maxsubarraysum(arr,n);
+    return 0;
+}
