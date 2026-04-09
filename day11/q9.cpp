64109b9a5d7bafebf725058fcd74299989c9e18d
kranti-sable <krantisable3@gmail.com>
2026-04-09 16:51:03 +0530

    lecture code

---
+++ day11/q9.cpp
@@ -0,0 +1,17 @@
+//inbult sort in desending order
+#include<iostream>
+#include<algorithm>
+using namespace std;
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<",";
+    }
+    cout<<endl;
+}
+int main(){
+    int arr[8]={1,4,1,3,2,4,3,7};
+    int n=8;
+    sort(arr,arr+8,greater<int>());
+    print(arr,8);
+    return 0;
+}
