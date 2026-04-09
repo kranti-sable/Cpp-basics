cfc042a2f7f893f353f67fa013d3cae53cd8f8d8
kranti-sable <krantisable3@gmail.com>
2026-04-09 16:43:43 +0530

    lecture code

---
+++ day11/q8.cpp
@@ -0,0 +1,17 @@
+//inbuilt sort--
+#include <iostream>
+#include <algorithm>
+using namespace std;
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<",";
+    }
+    cout<<endl;
+}
+int main(){
+    int arr[8]={1,4,1,3,2,4,3,7};
+    int n=8;
+    sort (arr,arr+8);
+    print(arr,n);
+    return 0;
+}
