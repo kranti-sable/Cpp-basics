40315dad734f3928c337f4d3c1ccf7a5bb39c5f8
kranti-sable <krantisable3@gmail.com>
2026-05-02 22:29:44 +0530

    learnt about how memory work in vector

---
+++ D15-Vectors/vec-memory-implementation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>vec={1,2,3,4};
+    cout<<"size:"<<vec.size()<<endl;//4
+    cout<<"capacity:"<<vec.capacity()<<endl;//4
+    vec.push_back(5);//adding 5 in vector 
+    cout<<"size:"<<vec.size()<<endl;//5
+    cout<<"capacity:"<<vec.capacity()<<endl;//8
+    return 0;
+} 
