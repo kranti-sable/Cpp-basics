5b2bf57d6a7ddb475d506a9f7243f50da1c2036a
kranti-sable <krantisable3@gmail.com>
2026-05-09 20:46:31 +0530

    leart about left shift,right shift int binary shift operator

---
+++ D16-Bit_maniputation/Binary_shift_operator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+//left shift
+cout<<(7<<2)<<endl;//28
+cout<<(7*2^2)<<endl;//28 by formula
+
+//right shift
+cout<<(7>>2)<<endl;//1
+cout<<(7/2^2)<<endl;//1 by formula
+return 0;
+}
