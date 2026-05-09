67fe3e09d5c4e06861ebacc0f0031ce556464922
kranti-sable <krantisable3@gmail.com>
2026-05-09 20:33:19 +0530

    learnt about three type bitwise operators

---
+++ D16-Bit_maniputation/bitwise_operators.cpp
@@ -0,0 +1,13 @@
+//there are three types of bitwise operator
+#include<iostream>
+using namespace std;
+int main(){
+    //AND operator &
+    cout<<(3&5)<<endl;//1
+
+    //OR operator |
+    cout<<(3|5)<<endl;//7
+
+    //XOR ^
+    cout<<(3^5)<<endl;//6
+}
