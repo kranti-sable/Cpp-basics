d05691098897b276b0ce5821e0ed05b489cbfa8b
kranti-sable <krantisable3@gmail.com>
2026-05-16 21:04:58 +0530

    Getters and Setters

---
+++ D17-OPPs-part1/getter-setter.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Student{
+    string name;
+    float cgpa;
+    public:
+    void getpercentage(){
+        cout<<(cgpa*10)<<"%\n";
+    }
+    //setters
+    void setName(string nameVal){
+        name=nameVal;
+    }
+    void setcgpa(float cgpaVal){
+        cgpa=cgpaVal;
+    }
+    //getters
+    string getName(){
+        return name;
+    }
+    float getcgpa(){
+        return cgpa;
+    }
+};
+int main(){
+    Student s1;
+    s1.setName("Kranti");
+    s1.setcgpa(9.1);
+
+    cout<<s1.getName()<<endl;
+    cout<<s1.getcgpa()<<endl;
+    return 0;
+}
