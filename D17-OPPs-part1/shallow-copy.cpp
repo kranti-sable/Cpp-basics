dec21d83ea8dbfe188f255c284e791fc4218f655
kranti-sable <krantisable3@gmail.com>
2026-05-18 14:49:41 +0530

    learnt about shallow copy constructor

---
+++ D17-OPPs-part1/shallow-copy.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class car{
+    public:
+    string name;
+    string color;
+    int *mileage;
+
+    car(string name,string color){
+        this->name=name;
+        this->color=color;
+        mileage = new int ;
+        *mileage=12;
+    }
+    car(car &original){
+        cout<<"coping original to new..\n";
+        name=original.name;
+        color=original.color;
+        mileage=original.mileage;
+    }
+};
+int main(){
+    car c1("maruti800","blue");
+        car c2(c1);
+        cout<<c2.name<<endl;
+        cout<<c2.color<<endl;
+        cout<<*c2.mileage<<endl;
+        *c2.mileage=10;
+
+        cout<<*c1.mileage<<endl;
+        return 0;
+    
+}
