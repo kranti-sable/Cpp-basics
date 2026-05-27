c2bc1dfcb355da39c77ad6c6eccd5a0d1b74272a
kranti-sable <krantisable3@gmail.com>
2026-05-27 17:23:01 +0530

    finding factorial of number

---
+++ D19-Recursion/Calculating-factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+int main(){
+     int ans=factorial(5);
+     cout<<ans<<endl;//120
+     return 0;
+}
