31062c483b7c5bcfac7b024023d887b96d151994
kranti-sable <krantisable3@gmail.com>
2026-05-29 16:26:15 +0530

    Q- printing x to the power n in recursion by O(log n) time complexcity

---
+++ D19-Recursion/print-X-to-the-power-N-optimize.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int pow(int x,int n){
+    if(n==0){
+        return 1;
+    }
+    int halfpow=pow(x,n/2);
+    int halfpowSquare=halfpow*halfpow;
+    if(n%2!=0){
+        return x*halfpowSquare;
+    }
+    return halfpowSquare;
+}
+int main(){
+    cout<<pow(2,10)<<endl;
+    return 0;
+}
