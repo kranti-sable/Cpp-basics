51a722b63e21ded112b48f45b1b0b29ed8f68f15
kranti-sable <krantisable3@gmail.com>
2026-05-29 16:18:34 +0530

    Q- printing x to the power n in recursion by O(n) time complexcity

---
+++ D19-Recursion/print-X-to-the-power-N.cpp
@@ -0,0 +1,13 @@
+// By O(n) approach
+#include<iostream>
+using namespace std;
+int pow(int x,int n){
+    if(n==0){
+       return 1;
+    }
+    return x*pow(x,n-1);
+}
+int main(){
+    cout<<pow(2,10)<<endl;
+    return 0;
+}
